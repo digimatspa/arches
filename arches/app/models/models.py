afcd2d477fa060c04fd03425451e1d8247876d0e
pbitonto <paolo.bitonto@digimat.it>
2022-01-12 21:18:52 +0100

    Log email sending errors

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -1199,6 +1199,7 @@ def send_email_on_save(sender, instance, **kwargs):
                     instance.isread = True
                     instance.save()
         except Exception as err:
+            logger.exception("error sending email")
             logger = logging.getLogger(__name__)
             logger.warn(f"Email Server not correctly set up. See settings to configure: {err}")
     return False
